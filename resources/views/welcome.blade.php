91128e1ed29a058d3b5f31a4a6708e74b80a167c
tomohito.takemhoto <takemoto82@icloud.com>
2021-03-17 13:57:26 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center_jumbotron mt-4 row">
-        <div class="text-center col-sm-7">
-            <h1 class="heading">保有銘柄の四半期決算情報を記録しよう！</h1>
-            <ul class="top_button list-unstyled">
-                {{-- ユーザ登録ページへのリンク --}}
-                {!! link_to_route('signup.get', 'はじめての方はこちら', [], ['class' => 'signup_btn btn-primary btn-block']) !!}
-                <li class="login"><button type="button" class="login_btn"><strong>ログインはこちら</strong></button></li>
-            </ul>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center_jumbotron mt-4 row">
+            <div class="text-center col-sm-7">
+                <h1 class="heading">保有銘柄の四半期決算情報を記録しよう！</h1>
+                <ul class="top_button list-unstyled">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    {!! link_to_route('signup.get', 'はじめての方はこちら', [], ['class' => 'signup_btn btn-primary btn-block']) !!}
+                    <li class="login"><button type="button" class="login_btn"><strong>ログインはこちら</strong></button></li>
+                </ul>
+            </div>
+            <div class="col-sm-5">
+                <picture class="top-picture">
+                    <img class="img-fluid" src='{{ asset('storage/picture-top.png') }}'>
+                </picture>
+            </div>
         </div>
-        <div class="col-sm-5">
-            <picture class="top-picture">
-                <img class="img-fluid" src='{{ asset('storage/picture-top.png') }}'>
-            </picture>
-        </div>
-        <div class="attendance col-sm-9">
+    @endif
+    <div class="attendance col-sm-9"></div>
 @endsection
